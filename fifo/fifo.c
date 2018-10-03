bcdb4e9eb72735198d9c5857bfa24a7fcb98bdb7
beanef1t <beanef1t@debian.localdomain>
2018-10-03 15:57:29 -0700

    main(), reader(), err_printf()

---
+++ fifo/fifo.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdarg.h>
+
+#define BUF_SIZE 256
+
+int reader (char* fifo_pathname);
+int writer (char* fifo_pathname, char* file_pathname);
+int err_printf (char* format, ...);
+
+int main (int argc, char* argv[])
+{
+	char* fifo_pathname = "fifo";
+
+	if ((mkfifo (fifo_pathname, 0644) == -1) && (errno != EEXIST))
+	{
+		printf ("can't create fifo\n");
+
+		exit (EXIT_FAILURE);
+	}
+
+	int isreader = (argc == 1);
+	
+	if (isreader)
+		if (reader (fifo_pathname) == -1)
+		{
+			printf ("reader () error\n");
+
+			exit (EXIT_FAILURE);
+		}
+		
+	else
+		if (writer (fifo_pathname, argv[1]) == -1)
+		{
+			printf ("writer (%s) error\n", argv [1]);
+
+			exit (EXIT_FAILURE);
+		}
+
+	return 0;
+}
+
+int reader (char* fifo_pathname)
+{
+	int fifo_fd = -1;
+
+	if (fifo_fd = open (fifo_pathname, O_RDONLY) == -1)
+		return err_printf ("No such file or directory: \"%s\"\n", fifo_pathname);
+
+	char buf [BUF_SIZE] = {};
+
+	int read_result = -1;
+	
+	while (read_result != 0)
+	{
+		if (read_result = read (fifo_fd, &buf, BUF_SIZE) == -1)
+			return err_printf ("err while reading from fifo\n");
+		
+		if (write (STDOUT_FILENO, &buf, read_result) == -1)
+			return err_printf ("err while writing to stdout\n");
+	}
+}
+
+int err_printf (char* format, ...)
+{
+	int err_tmp = errno;
+
+	va_list ap;
+	va_start (ap, format);
+	
+	vprintf (format, ap);
+
+	va_end (ap);
+
+	errno = err_tmp;
+
+	return -1;
+}
