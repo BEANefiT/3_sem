510c0f1052cf2fd684c987d95bea44adcda18beb
beanef1t <beanef1t@debian.localdomain>
2018-10-04 02:46:33 -0700

    fixed logic expressions

--- fifo/fifo.c
+++ fifo/fifo.c
@@ -11,13 +11,13 @@
 
 int reader (char* fifo_pathname);
 int writer (char* fifo_pathname, char* target_pathname);
-int err_printf (char* format, ...);
+int err_printf (int first_fd, int second_fd, char* format, ...);
 
 int main (int argc, char* argv[])
 {
-	char* fifo_pathname = "fifo";
+	char* fifo_pathname = "tmp_fifo";
 
-	if ((mkfifo (fifo_pathname, 0644) == -1) && (errno != EEXIST))
+	if ((mkfifo (fifo_pathname, 0664) == -1) && (errno != EEXIST))
 	{
 		printf ("can't create fifo\n");
 
@@ -27,20 +27,24 @@ int main (int argc, char* argv[])
 	int isreader = (argc == 1);
 	
 	if (isreader)
+	{
 		if (reader (fifo_pathname) == -1)
 		{
 			printf ("reader () error\n");
 
 			exit (EXIT_FAILURE);
 		}
-		
+	}	
+
 	else
+	{
 		if (writer (fifo_pathname, argv[1]) == -1)
 		{
 			printf ("writer (%s) error\n", argv [1]);
 
 			exit (EXIT_FAILURE);
 		}
+	}
 
 	return 0;
 }
@@ -48,36 +52,43 @@ int main (int argc, char* argv[])
 int reader (char* fifo_pathname)
 {
 	int fifo_fd = -1;
-
-	if (fifo_fd = open (fifo_pathname, O_RDONLY) == -1)
-		return err_printf ("No such file or directory: \"%s\"\n", fifo_pathname);
+	
+	if ((fifo_fd = open (fifo_pathname, O_RDONLY)) == -1)
+			return err_printf (fifo_fd, -1, "No such file or directory: \"%s\"\n", fifo_pathname);
 
 	char buf [BUF_SIZE] = {};
 
 	int read_result = -1;
 	
 	while (read_result != 0)
-	{
-		if (read_result = read (fifo_fd, &buf, BUF_SIZE) == -1)
-			return err_printf ("err while reading from fifo\n");
-		
-		if (write (STDOUT_FILENO, &buf, read_result) == -1)
-			return err_printf ("err while writing to stdout\n");
-	}
+	{	
+		if ((read_result = read (fifo_fd, &buf, BUF_SIZE)) == -1)
+			return err_printf (fifo_fd, -1,
+					   "err while reading from fifo\n");
 
+		if (write (STDOUT_FILENO, &buf, read_result) == -1)
+			return err_printf (fifo_fd, -1, "err while writing to stdout\n");
+	}
+		
 	close (fifo_fd);
+
+	return 0;
 }
 
 int writer (char* fifo_pathname, char* target_pathname)
 {
 	int fifo_fd 	= -1;
 	int target_fd 	= -1;
+	
+	if ((target_fd = open (target_pathname, O_RDONLY)) == -1)
+		return err_printf (fifo_fd, target_fd,
+				   "No such file or directory: \"%s\"\n",
+				   target_pathname);
 
-	if (fifo_fd = open (fifo_pathname, O_WRONLY) == -1)
-		return err_printf ("No such file or directory: \"%s\"\n", fifo_pathname);
-
-	if (target_fd = open (target_pathname, O_WRONLY) == -1)
-		return err_printf ("No such file or directory: \"%s\"\n", target_pathname);
+	if ((fifo_fd = open (fifo_pathname, O_WRONLY)) == -1)
+		return err_printf (fifo_fd, target_fd,
+			   	   "No such file or directory: \"%s\"\n",
+			   	   fifo_pathname);
 
 	char buf [BUF_SIZE] = {};
 
@@ -85,20 +96,27 @@ int writer (char* fifo_pathname, char* target_pathname)
 
 	while (read_result != 0)
 	{
-		if (read_result = read (target_fd, &buf, BUF_SIZE) == -1)
-			return err_printf ("err while reading from file\n");
-
+		if ((read_result = read (target_fd, &buf, BUF_SIZE)) == -1)
+			return err_printf (fifo_fd, target_fd,
+			   		   "err while reading from file\n");
+	
 		if (write (fifo_fd, &buf, read_result) == -1)
-			return err_printf ("err while writing to fifo\n");
+			return err_printf (fifo_fd, target_fd,
+					   "err while writing to fifo\n");
 	}
 
 	close (fifo_fd);
 	close (target_fd);
+
+	return 0;
 }
 
-int err_printf (char* format, ...)
+int err_printf (int first_fd, int second_fd, char* format, ...)
 {
 	int err_tmp = errno;
+
+	(first_fd == -1)  || close (first_fd);
+	(second_fd == -1) || close (second_fd);
 
 	va_list ap;
 	va_start (ap, format);
