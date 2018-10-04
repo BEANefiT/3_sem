361647b0883d423c8281e09434850e07218bf3fb
beanef1t <beanef1t@debian.localdomain>
2018-10-03 17:52:37 -0700

    writer()

--- fifo/fifo.c
+++ fifo/fifo.c
@@ -5,11 +5,12 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 256
 
 int reader (char* fifo_pathname);
-int writer (char* fifo_pathname, char* file_pathname);
+int writer (char* fifo_pathname, char* target_pathname);
 int err_printf (char* format, ...);
 
 int main (int argc, char* argv[])
@@ -65,6 +66,34 @@ int reader (char* fifo_pathname)
 	}
 
 	close (fifo_fd);
+}
+
+int writer (char* fifo_pathname, char* target_pathname)
+{
+	int fifo_fd 	= -1;
+	int target_fd 	= -1;
+
+	if (fifo_fd = open (fifo_pathname, O_WRONLY) == -1)
+		return err_printf ("No such file or directory: \"%s\"\n", fifo_pathname);
+
+	if (target_fd = open (target_pathname, O_WRONLY) == -1)
+		return err_printf ("No such file or directory: \"%s\"\n", target_pathname);
+
+	char buf [BUF_SIZE] = {};
+
+	int read_result = -1;
+
+	while (read_result != 0)
+	{
+		if (read_result = read (target_fd, &buf, BUF_SIZE) == -1)
+			return err_printf ("err while reading from file\n");
+
+		if (write (fifo_fd, &buf, read_result) == -1)
+			return err_printf ("err while writing to fifo\n");
+	}
+
+	close (fifo_fd);
+	close (target_fd);
 }
 
 int err_printf (char* format, ...)
