883392f76f115e1020adb8ad249ffdb75f294faf
beanef1t <beanef1t@debian.localdomain>
2018-11-24 13:57:41 -0800

    consumer and producer work in series in order to avoid deadlocks

--- semaphore/semaphore.c
+++ semaphore/semaphore.c
@@ -16,7 +16,6 @@
 #define NSOPS 4
 #define SHMKEY 22
 #define SHMSIZE 32
-#define MAXTIME 200
 
 int err_printf (char* format, ...);
 int producer (char* pathname);
@@ -29,41 +28,41 @@ struct sembuf sops [NSOPS] = {};
 
 int main (int argc, char* argv[])
 {
-	int is_producer = (argc == 2);
-	
-	if (argc > 2)
-		return err_printf ("Too many arguments\n");
+    int is_producer = (argc == 2);
 
-	if ((semid = semget (SEMKEY, SEMNUM, IPC_CREAT | 0666)) == -1)
-		return err_printf ("Can't create semaphore's set\n");
+    if (argc > 2)
+        return err_printf ("Too many arguments\n");
 
-	if ((shmid = shmget (SHMKEY, SHMSIZE + 2 * sizeof (int), IPC_CREAT | 0666)) == -1)
-		return err_printf ("Can't create shared memory\n");
+    if ((semid = semget (SEMKEY, SEMNUM, IPC_CREAT | 0666)) == -1)
+        return err_printf ("Can't create semaphore's set\n");
 
-	if ((shmaddr = shmat (shmid, NULL, 0)) == (void *) -1)
-		return err_printf ("Can't attach shared memory\n");
+    if ((shmid = shmget (SHMKEY, SHMSIZE + 8, IPC_CREAT | 0666)) == -1)
+        return err_printf ("Can't create shared memory\n");
 
-	if (is_producer)
-	{
-		if (producer (argv[1]) == -1)
-		{
-			fprintf (stderr, "producer error\n");
-			exit (EXIT_FAILURE);
-		}
-	}
+    if ((shmaddr = shmat (shmid, NULL, 0)) == (void *) -1)
+        return err_printf ("Can't attach shared memory\n");
 
-	else
-	{
-		if (consumer () == -1)
-		{
-			fprintf (stderr, "consumer error\n");
-			exit (EXIT_FAILURE);
-		}
-	}
+    if (is_producer)
+    {
+        if (producer (argv[1]) == -1)
+        {
+            fprintf (stderr, "producer error\n");
+            exit (EXIT_FAILURE);
+        }
+    }
 
-	shmctl (shmid, IPC_RMID, NULL);
-	
-	return 0;
+    else
+    {
+        if (consumer () == -1)
+        {
+            fprintf (stderr, "consumer error\n");
+            exit (EXIT_FAILURE);
+        }
+    }
+
+    shmctl (shmid, IPC_RMID, NULL);
+
+    return 0;
 }
 
 #define BYTES_WRITTEN ((int *)shmaddr)[0]
@@ -71,185 +70,194 @@ int main (int argc, char* argv[])
 #define SHMRD 1
 #define SHMWR 2
 
-#define SEMPUSH( num, op, flg )		\
-do					\
-{					\
-	sops[counter].sem_num = num;	\
-	sops[counter].sem_op  = op;	\
-	sops[counter++].sem_flg = flg;	\
+#define SEMPUSH( num, op, flg )     \
+do                                  \
+{                                   \
+    sops[counter].sem_num = num;    \
+    sops[counter].sem_op  = op;     \
+    sops[counter++].sem_flg = flg;  \
 } while(0)
 
-#define SEMOP_NORET()					\
-do							\
-{							\
-	semop (semid, sops, counter);			\
-	counter = 0;					\
+#define SEMOP_NORET()               \
+do                                  \
+{                                   \
+    semop (semid, sops, counter);   \
+    counter = 0;                    \
 } while(0)
 
-#define SEMOP()						\
-do							\
-{							\
-	if (semop (semid, sops, counter) == -1)		\
-		return err_printf ("semop error\n");	\
-							\
-	counter = 0;					\
+#define SEMOP()                                 \
+do                                              \
+{                                               \
+    if (semop (semid, sops, counter) == -1)     \
+        return err_printf ("semop error\n");    \
+                                                \
+    counter = 0;                                \
 } while(0)
 
-#define SEMCHECK( num, val )			\
-({						\
-	SEMPUSH (num, -1 * val, IPC_NOWAIT);	\
-	SEMPUSH (num, 0, IPC_NOWAIT);		\
-	SEMPUSH (num, val, IPC_NOWAIT);		\
-						\
-	int res = semop (semid, sops, counter);	\
-	counter = 0;				\
-	res;					\
+#define SEMCHECK( num, val )                \
+({                                          \
+    SEMPUSH (num, -1 * val, IPC_NOWAIT);    \
+    SEMPUSH (num, 0, IPC_NOWAIT);           \
+    SEMPUSH (num, val, IPC_NOWAIT);         \
+                                            \
+    int res = semop (semid, sops, counter); \
+    counter = 0;                            \
+    res;                                    \
 })
 
-#define SHMCHECK( role )						\
-do									\
-{									\
-	if (SEMCHECK (0, 2) == 0)					\
-	{								\
-		SEMPUSH (0, -1, 0);					\
-		SEMOP();						\
-									\
-		errno = ECANCELED;					\
-									\
-		switch (role)						\
-		{							\
-			case SHMRD:					\
-			{						\
-				err_printf ("producer is dead:(\n");	\
-									\
-				break;					\
-			}						\
-									\
-			case SHMWR:					\
-			{						\
-				err_printf ("consumer is dead:(\n");	\
-									\
-				break;					\
-			}						\
-		}							\
-									\
-		return -1;						\
-	}								\
-									\
-	if ((SEMCHECK (0, 4) == 0) && (BYTES_WRITTEN == BYTES_READ))	\
-	{								\
-		SEMPUSH (0, -3, 0);					\
-		SEMOP();						\
-									\
-		return 0;						\
-	}								\
-									\
-	if (BYTES_WRITTEN == BYTES_READ)				\
-	{								\
-		if (SEMCHECK (1, 0) == 0)				\
-		{							\
-			SEMPUSH (1, 1, 0);				\
-			SEMOP();					\
-		}							\
-	}								\
-									\
-	else								\
-	{								\
-		SEMPUSH (1, -1, IPC_NOWAIT);				\
-		SEMOP_NORET();						\
-	}								\
-									\
-	if ((BYTES_WRITTEN - BYTES_READ) >= SHMSIZE)			\
-	{								\
-		if (SEMCHECK (2, 0) == 0)				\
-		{							\
-			SEMPUSH (2, 1, 0);				\
-			SEMOP();					\
-		}							\
-	}								\
-									\
-	else								\
-	{								\
-		SEMPUSH (2, -1, IPC_NOWAIT);				\
-		SEMOP_NORET();						\
-	}								\
-									\
+#define SHMCHECK( role )                                            \
+do                                                                  \
+{                                                                   \
+    if (SEMCHECK (0, 2) == 0)                                       \
+    {                                                               \
+        errno = ECANCELED;                                          \
+                                                                    \
+        switch (role)                                               \
+        {                                                           \
+            case SHMRD:                                             \
+            {                                                       \
+                return err_printf ("producer is dead:(\n");         \
+            }                                                       \
+                                                                    \
+            case SHMWR:                                             \
+            {                                                       \
+                return err_printf ("consumer is dead:(\n");         \
+            }                                                       \
+        }                                                           \
+                                                                    \
+        return -1;                                                  \
+    }                                                               \
+                                                                    \
+    if ((SEMCHECK (0, 5) == 0) && (BYTES_WRITTEN == BYTES_READ))    \
+    {                                                               \
+        SEMPUSH (0, -4, 0);                                         \
+        SEMPUSH (SHMWR, 1, 0);                                      \
+        SEMOP();                                                    \
+                                                                    \
+        return 0;                                                   \
+    }                                                               \
+                                                                    \
 } while(0)
+
+/*
+    ALGORITHM:
+
+    switch (MAIN SEMAPHORE VALUE)
+    {
+        case 0: no pair producer + consumer
+
+        case 1: producer has access to shm and waits for consumer
+
+        case 2: producer or consumer is dead
+
+        case 4: there is a pair producer + consumer
+
+        case 5: producer done and exited, consumer is still obtaining from shm
+    }
+*/
 
 int producer (char* pathname)
 {
-	int counter = 0;
+    int counter = 0;
 
-	SEMPUSH (0, 0, 0);
-	SEMPUSH (0, 1, SEM_UNDO);
-	SEMOP();			//producers enters if there is no other producer/consumer
+    SEMPUSH (0, 0, 0);
+    SEMPUSH (0, 2, SEM_UNDO);
+    SEMPUSH (0, -1, 0);
+    SEMOP();            //producers enters if there is no other producer/consumer
 
-	SEMPUSH (0, -3, 0);
-	SEMPUSH (0, 0, 0);
-	SEMPUSH (0, 3, 0);
-	SEMOP();			//producers waits for consumer
+    SEMPUSH (0, -4, 0);
+    SEMPUSH (0, 0, 0);
+    SEMPUSH (0, 4, 0);
+    SEMOP();            //producers waits for consumer
 
-	BYTES_WRITTEN	= 0;
-	BYTES_READ	= 0;
-	SEMPUSH (SHMWR, -1, IPC_NOWAIT);
-	SEMOP_NORET();
+    BYTES_WRITTEN   = 0;
+    BYTES_READ  = 0;
+    SEMPUSH (SHMWR, -1, IPC_NOWAIT);
+    SEMOP_NORET();
 
-	int filefd = -1;
+    int filefd = -1;
 
-	if ((filefd = open (pathname, O_RDONLY)) == -1)
-		return err_printf ("can't open %s\n", pathname);
+    if ((filefd = open (pathname, O_RDONLY)) == -1)
+        return err_printf ("can't open %s\n", pathname);
 
-	int read_result = -1;
-	
-	while (read_result != 0)
-	{
-		SHMCHECK (SHMWR);
-		SEMPUSH (SHMWR, 0, 0);
-		SEMOP();
+    int read_result = -1;
 
-		void* buf = shmaddr + 8 + BYTES_WRITTEN % SHMSIZE;
+    while (1)
+    {
+        SHMCHECK (SHMWR);
+        SEMPUSH (SHMWR, 0, 0);
+        SEMPUSH (SHMWR, 1, 0);
+        SEMPUSH (SHMRD, 1, SEM_UNDO);
+        SEMPUSH (SHMRD, -1, 0);
+        SEMOP();
 
-		if ((read_result = read (filefd, buf, SHMSIZE - (BYTES_WRITTEN - BYTES_READ))) == -1)
-			return err_printf ("err while read() from filen\n");
+        void* buf = shmaddr + 8 + BYTES_WRITTEN % SHMSIZE;
 
-		BYTES_WRITTEN += read_result;
-	}
+        int size_available = SHMSIZE - (BYTES_WRITTEN - BYTES_READ);
 
-	SEMPUSH (0, 2, 0);
-	SEMPUSH (0, -1, SEM_UNDO);
-	SEMOP();			//successful exit, main semaphore value = 4
-					//if producer dies before this time, main_sem_val = 2
-	SEMPUSH (SHMRD, -1, IPC_NOWAIT);
-	SEMOP_NORET();
+        if ((read_result = read (filefd, buf, size_available)) == -1)
+            return err_printf ("err while read() from filen\n");
 
-	return 0;
+        if ((size_available != 0) && (read_result == 0))
+        {
+            SEMPUSH (SHMRD, 1, 0);
+            SEMOP();
+            break;
+        }
+
+        BYTES_WRITTEN += read_result;
+
+        SEMPUSH (SHMRD, 1, 0);
+        SEMPUSH (SHMRD, -1, SEM_UNDO);
+        SEMPUSH (SHMRD, -1, 0);
+        SEMOP();
+    }
+
+    SEMPUSH (0, 3, 0);
+    SEMPUSH (0, -2, SEM_UNDO);
+    SEMOP();            //successful exit, main semaphore value = 5
+                    //if producer dies before this time, main_sem_val = 2
+    SEMPUSH (SHMRD, -1, IPC_NOWAIT);
+    SEMOP_NORET();
+
+    return 0;
 }
 
 int consumer ()
 {
-	int counter = 0;
+    int counter = 0;
 
-	SEMPUSH (0, -1, 0);
-	SEMPUSH (0, 0, 0);
-	SEMPUSH (0, 2, 0);
-	SEMPUSH (0, 1, SEM_UNDO);
-	SEMOP();			//consumer waits for free producer
+    SEMPUSH (0, -1, 0);
+    SEMPUSH (0, 0, 0);
+    SEMPUSH (0, 2, 0);
+    SEMPUSH (0, 2, SEM_UNDO);
+    SEMOP();            //consumer waits for free producer
 
-	int write_result = -1;
+    int write_result = -1;
 
-	while (1)
-	{
-		SHMCHECK (SHMRD);
-		SEMPUSH (SHMRD, 0, 0);
-		SEMOP();
+    while (1)
+    {
+        SHMCHECK (SHMRD);
+        SEMPUSH (SHMRD, 0, 0);
+        SEMPUSH (SHMRD, 1, 0);
+        SEMPUSH (SHMWR, 1, SEM_UNDO);
+        SEMPUSH (SHMWR, -1, 0);
+        SEMOP();
 
-		void* buf = shmaddr + 8 + BYTES_READ % SHMSIZE;
+        void* buf = shmaddr + 8 + BYTES_READ % SHMSIZE;
 
-		if ((write_result = write (STDOUT_FILENO, buf, BYTES_WRITTEN - BYTES_READ)) == -1)
-			return err_printf ("err while write() to stdout\n");
+        int size_available = BYTES_WRITTEN - BYTES_READ;
 
-		BYTES_READ += write_result;
-	}
+        if ((write_result = write (STDOUT_FILENO, buf, size_available)) == -1)
+            return err_printf ("err while write() to stdout\n");
+
+        BYTES_READ += write_result;
+
+        SEMPUSH (SHMWR, 1, 0);
+        SEMPUSH (SHMWR, -1, SEM_UNDO);
+        SEMPUSH (SHMWR, -1, 0);
+        SEMOP();
+    }
 }
 
 #undef BYTES_WRITTEN
@@ -265,26 +273,26 @@ int consumer ()
 
 #ifdef __GNUC__
 
-	int err_printf (char *format, ...) __attribute__ ((format(printf, 1, 2)));
+    int err_printf (char *format, ...) __attribute__ ((format(printf, 1, 2)));
 
 #endif /*__GNUC__*/
 
 int err_printf (char* format, ...)
 {
-	int err_tmp = errno;
+    int err_tmp = errno;
 
-	(shmid == -1) || shmctl (shmid, IPC_RMID, NULL);
+    (shmid == -1) || shmctl (shmid, IPC_RMID, NULL);
 
-	va_list ap;
-	va_start (ap, format);
-	vfprintf (stderr, format, ap);
-	va_end (ap);
+    va_list ap;
+    va_start (ap, format);
+    vfprintf (stderr, format, ap);
+    va_end (ap);
 
-	errno = err_tmp;
+    errno = err_tmp;
 
-	perror (NULL);
+    perror (NULL);
 
-	return -1;
+    return -1;
 }
 
 #undef SEMKEY
