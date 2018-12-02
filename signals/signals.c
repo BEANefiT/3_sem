3b8cf898edd527076d944fccf93f97eae9042d43
beanef1t <beanef1t@debian.localdomain>
2018-12-02 07:27:29 -0800

    use sigsuspend() with alarm() instead of sigtimedwait()

--- signals/signals.c
+++ signals/signals.c
@@ -10,15 +10,16 @@
 #include <stdlib.h>
 
 #define BUFSIZE 32
-#define TIME (const struct timespec[]){{0, 5 * 1e8L}}
 
-void    nop         (int signum){}
+void    nop         (int signum) {}
+void    catch_bit   (int signum);
 void    die         (int signum);
 int     receive     (pid_t pid, void* buf, size_t count);
 int     send        (pid_t pid, void* buf, size_t count);
 int     err_printf  (char* format, ...);
 
-sigset_t set = {};
+sigset_t    set = {};
+char        symb = 0;
 
 #define SIGACTION( signum, foo )                                        \
 do                                                                      \
@@ -28,13 +29,22 @@ do                                                                      \
         return err_printf ("can't change disposition of"#signum"\n");   \
 } while(0)
 
+#define SIGSUSPEND( set, time ) \
+do                              \
+{                               \
+    alarm (time);               \
+    sigsuspend (set);           \
+    alarm (0);                  \
+} while (0)
+
 int main (int argc, char *argv[])
 {
-    if (argc < 2)
-        return err_printf ("Enter argument\n");
+    if (argc != 2)
+    {   
+        errno = EINVAL;
 
-    if (argc > 2)
-        return err_printf ("Too many arguments\n");
+        return err_printf ("Usage: %s {filename}\n", argv[0]);
+    }
 
     pid_t pid   = -1;
     pid_t ppid  = getpid();
@@ -43,22 +53,29 @@ int main (int argc, char *argv[])
 
     SIGACTION (SIGCHLD, die);
 
-    if (sigemptyset (&set)          == -1 ||
-        sigaddset   (&set, SIGUSR1) == -1 ||
-        sigaddset   (&set, SIGUSR2) == -1)
+    if (sigfillset (&set)           == -1 ||
+        sigdelset   (&set, SIGINT)  == -1 ||
+        sigdelset   (&set, SIGCHLD) == -1 ||
+        sigdelset   (&set, SIGALRM) == -1)
     {
         return err_printf ("can't edit set\n");
     }
 
     sigprocmask(SIG_SETMASK, &set, NULL);
 
+    if (sigdelset   (&set, SIGUSR1) == -1 ||
+        sigdelset   (&set, SIGUSR2) == -1)
+    {
+        return err_printf ("can't edit set\n");
+    }
+
     if((pid = fork ()) == -1)
         return err_printf ("can't fork new process\n");
 
     if (pid != 0)
     {
-        //SIGACTION (SIGUSR1, nop);
-        //SIGACTION (SIGUSR2, nop);
+        SIGACTION (SIGUSR1, catch_bit);
+        SIGACTION (SIGUSR2, catch_bit);
 
         char    buf [BUFSIZE] = {};
 
@@ -75,11 +92,16 @@ int main (int argc, char *argv[])
             if (write (STDOUT_FILENO, buf, nbytes) == -1)
                 return err_printf ("can't write to stdout\n");
         }
+
+        SIGACTION (SIGCHLD, SIG_DFL);
+
+        if (kill (pid, SIGUSR1) == -1)
+            return err_printf ("EXIT_FAILURE\n");
     }
 
     if (pid == 0)
     {
-        //SIGACTION (SIGUSR1, nop);
+        SIGACTION (SIGUSR1, nop);
 
         char    buf [BUFSIZE] = {};
         size_t  nbytes  = -1;
@@ -99,13 +121,12 @@ int main (int argc, char *argv[])
             if (send (ppid, buf, nbytes) == -1)
                 return err_printf ("can't send bits\n");
         }
+
+        SIGSUSPEND (&set, 1);
     }
 
     return 0;
 }
-
-#undef BUFSIZE
-#undef SIGACTION
 
 void die (int signum)
 {
@@ -115,22 +136,25 @@ void die (int signum)
     exit (EXIT_FAILURE);
 }
 
+void catch_bit (int signum)
+{
+    switch (signum)
+    {
+        case SIGUSR1:   {symb += 0; break;}
+
+        case SIGUSR2:   {symb += 1; break;}
+    }
+}
+
 int receive (pid_t pid, void* buf, size_t count)
 {
     for (size_t i = 0; i < count; i++)
     {
-        char symb = 0;
+        symb = 0;
 
         for (int j = 0; j < 8; j++)
         {
-            switch (sigtimedwait (&set, NULL, TIME))
-            {
-                case SIGUSR1:   {symb += 0; break;}
-
-                case SIGUSR2:   {symb += 1; break;}
-
-                case (-1):      {return -1;}
-            }
+            SIGSUSPEND (&set, 1);
 
             if (j != 7) {symb <<= 1;}
 
@@ -148,26 +172,39 @@ int send (pid_t pid, void* buf, size_t count)
 {
     for (size_t i = 0; i < count; i++)
     {
-        char symb = ((char *) buf) [i];
+        symb = ((char *) buf) [i];
 
         for (int j = 0; j < 8; j++)
         {
             switch ((symb & (1 << 7)) >> 7)
             {
-                case 0: {if (kill (pid, SIGUSR1) == -1) return -1; break;}
+                case 0: {
+                            if (kill (pid, SIGUSR1) == -1)
+                                return -1;
 
-                case 1: {if (kill (pid, SIGUSR2) == -1) return -1; break;}
+                            break;
+                        }
+
+                case 1: {
+                            if (kill (pid, SIGUSR2) == -1)
+                                return -1;
+
+                            break;
+                        }
             }
 
             symb <<= 1;
 
-            if (sigtimedwait (&set, NULL, TIME) == -1)
-                return -1;
+            SIGSUSPEND (&set, 1);
         }
     }
 
     return count;
 }
+
+#undef BUFSIZE
+#undef SIGACTION
+#undef SIGSUSPEND
 
 #ifdef __GNUC__
 
